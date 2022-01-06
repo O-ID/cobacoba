83bf525552c58ddee6cab7fae74efee77a04ef85
O-ID <odydahary1@gmail.com>
2022-01-06 10:05:01 +0700

    upadate

---
+++ dist/js/about.ad1f7191.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,a,e){"use strict";e.r(a);var t=e("7a23"),o={class:"about"},u=Object(t["e"])("h1",null,"This is an about page ody app vue",-1),c=[u];function s(n,a){return Object(t["q"])(),Object(t["d"])("div",o,c)}var b=e("6b0d"),d=e.n(b);const i={},p=d()(i,[["render",s]]);a["default"]=p}}]);
+//# sourceMappingURL=about.ad1f7191.js.map
